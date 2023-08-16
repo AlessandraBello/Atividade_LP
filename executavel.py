fc60c61a5b42ad6d729bfa0bf1f059e8657bacbe
AlessandraBello <126773287+AlessandraBello@users.noreply.github.com>
2023-08-16 08:55:07 -0300

    Exemplos 3

--- executavel.py
+++ executavel.py
@@ -8,8 +8,16 @@ array2 = Alessandra.gerar_ndarray(0,100,10)
 array3 = Alessandra.somar(array1, array2)
 
 #exercicio 2 
-array3 = Alessandra.redimensionar_array (array3)
+array3 = Alessandra.redimensionar_array (array3, 2,5)
 
 array3 = Alessandra.converter_float(array3)
 
 array3 = Alessandra.transpostas(array3)
+
+#exercicio3
+
+array4 = Alessandra.gerar_ndarray(0,100,10)
+
+array4 = Alessandra.redimensionar_array (array4, 5, 2)
+
+array4 = Alessandra.multiplicar(array3, array4)
