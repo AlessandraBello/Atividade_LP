e19c7384208039af7924610dc84a9ff2f3237ced
AlessandraBello <126773287+AlessandraBello@users.noreply.github.com>
2023-08-16 08:43:19 -0300

    exercicio 2

--- executavel.py
+++ executavel.py
@@ -8,3 +8,8 @@ array2 = Alessandra.gerar_ndarray(0,100,10)
 array3 = Alessandra.somar(array1, array2)
 
 #exercicio 2 
+array3 = Alessandra.redimensionar_array (array3)
+
+array3 = Alessandra.converter_float(array3)
+
+array3 = Alessandra.transpostas(array3)
